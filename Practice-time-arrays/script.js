fa81a37c078176073a4dc8ba959e254779d2bbe6
laineleo <lalaineligao067@gmail.com>
2025-03-04 15:54:51 +0800

    commit

---
+++ Practice-time-arrays/script.js
@@ -0,0 +1,20 @@
+//1.
+let dailyTasks= ['reading', 'writing', 'coding', 'exercising', 'singing'];
+
+//2.
+let dailyTasks= ['reading', 'writing', 'coding', 'exercising', 'singing'];
+
+console.log(dailyTasks[0]);
+console.log(dailyTasks[4]);
+
+//3.
+let dailyTasks= ['reading', 'writing', 'coding', 'exercising', 'singing'];
+dailyTasks.push('sleeping');
+
+console.log(dailyTasks);
+
+//4.
+let dailyTasks= ['reading', 'writing', 'coding', 'exercising', 'singing'];
+dailyTasks.shift();
+
+console.log(dailyTasks);
