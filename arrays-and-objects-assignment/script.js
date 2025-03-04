9fb3859f8f909c35ed102557ce4ffd35de32c9d1
laineleo <lalaineligao067@gmail.com>
2025-03-04 21:54:36 +0800

    commit

--- arrays-and-objects-assignment/script.js
+++ arrays-and-objects-assignment/script.js
@@ -1,97 +1,97 @@
 //Task 1
     //#1-2 instructions
-        // let book={
-        //     title: "The Alchemist",
-        //     author:"Paulo Coelho",
-        //     pages: 200,
-        //     isRead: true,
-        // };
+        let book={
+            title: "The Alchemist",
+            author:"Paulo Coelho",
+            pages: 200,
+            isRead: true,
+        };
 
-        // console.log(book.title);
-        // console.log(book.author);
-        // console.log(book.pages);
-        // console.log(book.isRead);
+        console.log(book.title);
+        console.log(book.author);
+        console.log(book.pages);
+        console.log(book.isRead);
 
-//     //#3-4 instructions
-    // let book={
-    //     title: "The Alchemist",
-    //     author:"Paulo Coelho",
-    //     pages: 200,
-    //     isRead: true,
-    // };
+    //#3-4 instructions
+    let book={
+        title: "The Alchemist",
+        author:"Paulo Coelho",
+        pages: 200,
+        isRead: true,
+    };
 
-    // book.genre= "Fable";
-    // console.log(book);
+    book.genre= "Fable";
+    console.log(book);
 
-// //Task 2
-//     let movies= [
-//         {
-//             title: "Doctor Strange in the Multiverse of Madness",
-//             Director: "Sam Raimi",
-//             year: 2022
-//         },
-//         {
-//             title: "Spider-Man: No Way Home",
-//             director: "Jon Watts",
-//             year: 2021
-//         },
-//         {
-//             title: "Black Panther: Wakanda Forever",
-//             director: "Ryan Coogler",
-//             year: 2022
-//         }
-//     ];
+//Task 2
+    let movies= [
+        {
+            title: "Doctor Strange in the Multiverse of Madness",
+            Director: "Sam Raimi",
+            year: 2022
+        },
+        {
+            title: "Spider-Man: No Way Home",
+            director: "Jon Watts",
+            year: 2021
+        },
+        {
+            title: "Black Panther: Wakanda Forever",
+            director: "Ryan Coogler",
+            year: 2022
+        }
+    ];
 
-//#2 instruction
-//     console.log(movies[1].title);
-
-//     //#3 instruction
-//     movies.push({
-//         title: "Guardians of the Galaxy Vol. 3",
-//         director: "James Gunn",
-//         year: 2023
-//     });
-
-//     //#4 instruction
-//     movies[0].year= 2023;
-
-//     //#5 instruction
-//     console.log(movies);
-
-// //Task 3
-    // let student= {
-    //     name: "Lalaine",
-    //     age: 22,
-    //     subjects: ['Math', 'English', 'Science']
-    // };
     //#2 instruction
-    // console.log(student.subjects[0]);
+    console.log(movies[1].title);
 
     //#3 instruction
-    // student.subjects.push('History');
-    // console.log(student.subjects);
+    movies.push({
+        title: "Guardians of the Galaxy Vol. 3",
+        director: "James Gunn",
+        year: 2023
+    });
 
     //#4 instruction
-    // console.log(student);
+    movies[0].year= 2023;
 
-// //Task 4
-let recipe= {
-    name: "Pasta Salad",
-    ingredients: [{
-        name: "Pasta",
-        quantity: "1 cup"
-    }],
-    isVegetarian: "false"
-};
-//#2 instruction
-recipe.ingredients.push({
-    name: "Nestle Cream",
-    quantity: "1 pc.",
-});
-// console.log(recipe.ingredients);
+    //#5 instruction
+    console.log(movies);
 
-//#3 instruction
-// console.log(recipe.ingredients[1]);
+//Task 3
+    let student= {
+        name: "Lalaine",
+        age: 22,
+        subjects: ['Math', 'English', 'Science']
+    };
+    //#2 instruction
+    console.log(student.subjects[0]);
 
-// //#4
-console.log(recipe);
+    //#3 instruction
+    student.subjects.push('History');
+    console.log(student.subjects);
+
+    //#4 instruction
+    console.log(student);
+
+//Task 4
+    let recipe= {
+        name: "Pasta Salad",
+        ingredients: [{
+            name: "Pasta",
+            quantity: "1 cup"
+        }],
+        isVegetarian: "false"
+    };
+    //#2 instruction
+    recipe.ingredients.push({
+        name: "Nestle Cream",
+        quantity: "1 pc.",
+    });
+    console.log(recipe.ingredients);
+
+    //#3 instruction
+    console.log(recipe.ingredients[1]);
+
+    //#4
+    console.log(recipe);
---
+++ conditions-assignment/script.js
@@ -0,0 +1,76 @@
+//Task 1
+    // let score= 77;
+
+    // if (score >=90 && score<=100){
+    //     console.log("Grade A");
+    // }else if (score >=80 && score<=89){
+    //     console.log("Grade B");
+    // }else if(score>=70 && score<=79){
+    //     console.log("Grade C");
+    // }else if(score>=60 && score<=69){
+    //     console.log("Grade D");
+    // }else{
+    //     console.log("Grade F");
+    // }
+
+// Task 2
+    // let temperature=12;
+
+    // if(temperature<0){
+    //     console.log("It's freezing outside!Bundle up!");
+    // }else if(temperature>=0 && temperature<=15){
+    //     console.log("It's cold outside. Wear a jacket.");
+    // }else if (temperature>=16 && temperature<=30){
+    //     console.log("The weather is nice. Enjoy your day!");
+    // }else{
+    //     console.log("It's hot outside. Stay hydrated!");
+    // }
+    // console.log(`temperature: ${temperature}`);
+
+//Task 3
+    // let age= 17;
+
+    // if (age<13){
+    //     console.log("You are too young for this activity.");
+    // }else if (age>=13 && age<=17){
+    //     console.log("You need parental permission.");
+    // }else{
+    //     console.log("You are eligible for this activity.");
+    // }
+
+    // console.log(`Age: ${age}`);
+
+//Task 4
+    // let age=17;
+    // let isMember= true;
+    // let ticketPrice;
+    // if (age<12){
+    //     ticketPrice ="free.";
+    // }else if(age>=12 && isMember){
+    //     ticketPrice="$10.";
+    // }else{
+    //     ticketPrice= "$15.";
+    // }
+
+    // console.log(`Ticket Price: ${ticketPrice}`);
+
+//Task 5
+    function isLeapYear(year){
+        if (year % 4 === 0){
+            return true;
+        }else if (year % 100 === 0){
+            return false;
+        }else if (year % 400 === 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    
+    console.log(isLeapYear(2024));
+    console.log(isLeapYear(2021));
+    console.log(isLeapYear(2002));
+
+    
+
+
